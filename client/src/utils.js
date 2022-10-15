997aa051915a6860c1134487016914158e4da687
jaakjuu1 <juuso.jaakkola@gmail.com>
2022-10-15 20:16:05 +0300

    added detect Ethereum Provider

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,8 +1,9 @@
 import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider"
 import Dex from "./contracts/Dex.json";
 import ERC20Abi from "./ERC20Abi.json";
 
-const getWeb3 = () => {
+/* const getWeb3 = () => {
     return new Promise((resolve, reject) => {
         // Wait for loading completion to avoid race conditions with web3 injection timing.
         window.addEventListener("load", async () => {
@@ -37,6 +38,22 @@ const getWeb3 = () => {
         });
     });
 };
+ */
+
+const getWeb3 = () => 
+    new Promise(async (resolve, reject) => {
+        let provider = await detectEthereumProvider();
+        if (provider) {
+            await provider.request({ method: 'eth_requestAccounts' });
+            try {
+                const web3 = new Web3(window.ethereum);
+                resolve(web3);
+            } catch (error) {
+                reject(error);
+            }
+        } reject('Install Metamask');
+    });
+
 
 const getContracts = async web3 => {
     const networkId = await web3.eth.net.getId();
